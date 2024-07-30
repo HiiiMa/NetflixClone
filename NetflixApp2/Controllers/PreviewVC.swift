b20abdfac8143646330ffe031f8a98f2459628e9
ibrahim alasl <ibrahimalasal19@gmail.com>
2024-07-30 17:04:32 +0300

    Adding SwiftLoader

--- NetflixApp2/Controllers/PreviewVC.swift
+++ NetflixApp2/Controllers/PreviewVC.swift
@@ -6,6 +6,7 @@
 //
 import WebKit
 import UIKit
+import SwiftLoader
 
 final class PreviewVC: UIViewController {
     
@@ -13,13 +14,16 @@ final class PreviewVC: UIViewController {
     @IBOutlet private weak var movieTitle: UILabel!
     @IBOutlet private weak var preview: UILabel!
     @IBOutlet private weak var downloadButton: UIButton!
-//MARK: movieData is a public variable to get and object of type Title when a cell is tapped, the cell passes the object to the view controller and the view controller passes it when instantiating a new view controller of PreviewVC, then this object passess some of it's data to the "model" variable.
+    //MARK: movieData is a public variable to get and object of type Title when a cell is tapped, the cell passes the object to the view controller and the view controller passes it when instantiating a new view controller of PreviewVC, then this object passess some of it's data to the "model" variable.
     public var movieData: Title?
-//MARK: after getting some of the data from movieData and apicall is made to get the videoElement, then we call configure to fill up the outlets.
+    //MARK: after getting some of the data from movieData and apicall is made to get the videoElement, then we call configure to fill up the outlets.
     private var model: TitlePreviewModel?
+    //var spinner: UIActivityIndicatorView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // spinner = UIActivityIndicatorView(frame: view.frame)
+        showSwiftLoader()
         setupUI()
         getMovie()
     }
@@ -40,6 +44,8 @@ final class PreviewVC: UIViewController {
 }
 private extension PreviewVC {
     func getMovie(){
+        //showLoader()
+        SwiftLoader.show(animated: true)
         APICaller.shared.getMovie(with: (movieData?.original_title ?? "") + " trailer") { [weak self] result in
             switch result {
             case .success(let videoElement):
@@ -48,11 +54,60 @@ private extension PreviewVC {
                 DispatchQueue.main.async{[weak self] in
                     guard let self else{return }
                     configure()
+                    SwiftLoader.hide()
+                    // hideLoader()
                 }
             case .failure(let error):
+                DispatchQueue.main.async{[weak self] in
+                    guard self != nil else{return }
+                    self?.delay(seconds: 0.7) { () -> () in
+                        SwiftLoader.hide()
+                    }
+                    //hideLoader()
+                }
                 print(error.localizedDescription)
             }
             
         }
     }
 }
+extension PreviewVC{
+    func delay(seconds: Double, completion: @escaping () -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: completion)
+    }
+    @objc func showSwiftLoader() {
+        var config : SwiftLoader.Config = SwiftLoader.Config()
+        config.size = 170
+        config.backgroundColor = .black
+        config.spinnerColor = .white
+        config.titleTextColor = .white
+        config.spinnerLineWidth = 2.0
+        config.foregroundColor = UIColor.black
+        config.foregroundAlpha = 0.5
+        
+        SwiftLoader.setConfig(config)
+    }
+}
+//extension PreviewVC {
+//    func showLoader()  {
+//        // Customize as per your need
+//        guard let spinner else { return }
+//        spinner.backgroundColor = UIColor.black.withAlphaComponent(1)
+//        spinner.layer.cornerRadius = 3.0
+//        spinner.clipsToBounds = true
+//        spinner.hidesWhenStopped = true
+//        spinner.style = .medium // Corrected this line
+//        spinner.center = view.center
+//        view.addSubview(spinner)
+//        spinner.startAnimating()
+//        view.isUserInteractionEnabled = false // Disable interaction
+//    }
+//
+//    func hideLoader() {
+//        guard let spinner else { return }
+//        spinner.stopAnimating()
+//        spinner.removeFromSuperview()
+//        view.isUserInteractionEnabled = true // Re-enable interaction
+//    }
+//}
+//
